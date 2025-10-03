814b1c4935319ec13c16448c412f80038e6bd314
Scott <dkudviklerspg@gmail.com>
2025-10-03 21:08:55 +0200

    ds

--- frontend/src/context/WalletContext.tsx
+++ frontend/src/context/WalletContext.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactNode, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider as SolanaWalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import { clusterApiUrl } from '@solana/web3.js';
 
 // Import wallet adapter CSS
